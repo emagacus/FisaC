2ebd61ede711fc338af6f8064d9d9f1de2f144ce
emaga <emaga@DESKTOP-N4MONBD>
2017-04-03 06:43:12 -0500

    Funciona!

--- FisToC/Form1.cs
+++ FisToC/Form1.cs
@@ -11,75 +11,12 @@ using System.Windows.Forms;
 
 namespace FisToC
 {
-
-
-    public class memfunction
-    {
-        public int[] range;
-    }
-
-    public class Input
-        {
-        public Input()
-        {
-            mfs = new List<memfunction>();
-        }
-        public int Nmf;
-        public List<memfunction> mfs;
-        }
-
-    public class Output
-    {
-        public Output()
-        {
-            outval = new List<double>();
-        }
-
-       public List<double> outval;
-    }
-
-    public class Rules
-    {
-
-        public List<string> rulestring;
-        public List<int> inputIndex;
-        public List<int> OutputIndex;
-        public Rules() { rulestring = new List<string>(); }
-
-        public int[,] ruleArray(int nrules,int ninput, int noutput)
-        {
-            int columns = ninput + noutput;
-            int[,] reglas = new int[nrules,columns];
-
-            int filecount = 0;
-            foreach (string s in rulestring)
-            {
-                
-                string[] array = s.Split(' ');
-                for(int x=0;x<columns;x++)
-                {
-                    array[x] = array[x].Trim(',');
-                    Console.WriteLine(array[x]);
-                    reglas[filecount, x] = int.Parse(array[x]);
-                    
-                }
-                filecount++;
-                if (filecount == nrules) { break; }
-            }
-
-            Console.WriteLine(reglas.ToString());
-            return reglas;
-        } 
-
-    }
-
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
 
         private void processingText()
         {
@@ -103,21 +40,30 @@ namespace FisToC
                     for (int x = 0; x < inp.Nmf; x++)
                     {
                         string[] mfvalues = lineas[y].Replace('[', ' ').Replace(']', ' ').Split(' ');
-                        int[] v = new int [4];
-                         v[0] = Int32.Parse(mfvalues[1]);
-                         v[1] = Int32.Parse(mfvalues[2]);
-                         v[2] = Int32.Parse(mfvalues[3]);
-                         v[3] = Int32.Parse(mfvalues[4]);
+                        Console.WriteLine(mfvalues);
+                        double[] v = new double [4];
+                         v[0] = double.Parse(mfvalues[1]);
+                         v[1] = double.Parse(mfvalues[2]);
+                         v[2] = double.Parse(mfvalues[3]);
+                         v[3] = double.Parse(mfvalues[4]);
+
+                        Console.WriteLine("v[0] " + v[0]);
+                        Console.WriteLine("v[1] " + v[1]);
+                        Console.WriteLine("v[2] " + v[2]);
+                        Console.WriteLine("v[3] " + v[3]);
 
                         var mf = new memfunction();
                         mf.range = v;
                         inp.mfs.Add(mf);
-                        
+                        y++;
 
                     }   //añadir funciones de mebresia
 
                     inputs.Add(inp);
                     InputNumber++;
+
+
+
                 }
                 if (s.Contains("Output" + (Outputnumber + 1).ToString()))
                 {
@@ -125,10 +71,17 @@ namespace FisToC
 
                     string[] nmf = lineas[index + 3].Replace('=', ' ').Split(' '); //obtener el numero de funciones de membresia
                     int onmf = int.Parse(nmf[1]);
+                    
 
                     for (int x = 0; x < onmf; x++)
                     {
-                        ou.outval.Add(0);
+                        string[] mfo = lineas[index + 4 + x].Split('[');
+                        string[] mfo1 = mfo[1].Replace(']',' ').Split(' ');
+                       // Console.WriteLine("MF OUTPUT: " + mfo1[2]);
+                        //Sacar el valor de la x en la funcion linear
+
+                        // double val = mfo[];
+                        ou.outval.Add(double.Parse(mfo1[2]));
                     }
 
                         outputs.Add(ou);
@@ -186,6 +139,7 @@ namespace FisToC
             for (int i = 0; i < filas; i++)
             {
                 string sgetval = "double v" + i.ToString() + "[] = {";
+                int nmf = 0;
 
                 for (int j = 0; j < columnas - 1; j++)
                 {
@@ -194,7 +148,7 @@ namespace FisToC
                         textBox2.Text += "\n double a" + i.ToString() + j.ToString() + "[4] = {";
                         // textBox2.Text += "\n double r" + i.ToString() + "v" + j.ToString() + " = trapmf({";
 
-                        int[] param1 = inputs[j].mfs[arreglas[i, j] - 1].range;
+                        double[] param1 = inputs[j].mfs[arreglas[i, j] - 1].range;
                         for (int x = 0; x < param1.GetLength(0) - 1; x++)
                         {
                             textBox2.Text += param1[x].ToString() + ",";
@@ -204,19 +158,29 @@ namespace FisToC
                     if (arreglas[i, j] != 0)
                     {
                         textBox2.Text += "};";
-                        textBox2.Text += "\n double r" + i.ToString() + "v" + j.ToString() + " = trapmf(a" + i.ToString() + j.ToString() + ",1);";
+                        textBox2.Text += "\n double r" + i.ToString() + "v" + j.ToString() + " = trapmf(a" + i.ToString() + j.ToString() + ",input"+ j.ToString()+ ");";
                         sgetval += "r" + i.ToString() + "v" + j.ToString();
                         sgetval += ",";
-
+                        nmf++;
 
 
                     }//Console.Write(string.Format("{0} ", arreglas[i, j]));
                 }
                 sgetval = sgetval.Remove(sgetval.Length - 1);
                 sgetval += "};";
-                textBox2.Text += "\n"+sgetval+"\n";
-                textBox2.Text += "double res" +i.ToString() + " = getValue(" + "v" +i.ToString() + ",1);";
-               
+                textBox2.Text += "\n"+sgetval+"\n";                                                 //1 si es and 1 si es or
+                string nO = Reglas.ruleOrAnd[i].ToString();
+                textBox2.Text += "double res" +i.ToString() + " = getValue(" + "v" +i.ToString() + "," + nO + ","+ nmf.ToString() +");";
+                //getvalue saca el valor de cada regla
+            }
+
+
+            foreach(var o in outputs)
+            {
+                foreach(var i in o.outval)
+                {
+                    Console.WriteLine("Valor de output: " + i.ToString());
+                }
             }
 
             //filas es tambien el numero de reglas
@@ -225,13 +189,25 @@ namespace FisToC
             
             for (int x = 0; x < filas;x++)
             {
+                textBox2.Text += "res" + x.ToString() + "*" + outputs[0].outval[Reglas.RuleOutput[x]-1];
+                if (x != filas - 1) { textBox2.Text += ","; }
+            }
+
+           
+
+            textBox2.Text += "};";
+
+            textBox2.Text += "\n double mfres[] = {";
+
+            for (int x = 0; x < filas; x++)
+            {
                 textBox2.Text += "res" + x.ToString();
                 if (x != filas - 1) { textBox2.Text += ","; }
             }
             textBox2.Text += "};";
-            textBox2.Text += "\n double resultado = takagiInf(ress);";
-            textBox2.Text += "\n printf(el resultado es %lf,resultado);";
-
+            textBox2.Text += "\n double resultado = takagiInf(ress,mfres,"+ filas.ToString() + ");";
+            textBox2.Text += "\n printf(\"El resultado es %lf\",resultado);";
+            textBox2.Text += "\n }";
 
 
         }
@@ -262,4 +238,79 @@ namespace FisToC
             processingText();
         }
     }
+
+    public class memfunction
+    {
+        public double[] range;
+    }
+
+    public class Input
+    {
+        public Input()
+        {
+            mfs = new List<memfunction>();
+        }
+        public int Nmf;
+        public List<memfunction> mfs;
+    }
+
+    public class Output
+    {
+        public Output()
+        {
+            outval = new List<double>();
+        }
+
+        public List<double> outval;
+    }
+
+    public class Rules
+    {
+
+        public List<int> RuleOutput;
+        public List<int> ruleOrAnd;
+        public List<string> rulestring;
+        public List<int> inputIndex;
+        public List<int> OutputIndex;
+        public Rules() { rulestring = new List<string>(); }
+
+        public int[,] ruleArray(int nrules, int ninput, int noutput)
+        {
+            int columns = ninput + noutput;
+            int[,] reglas = new int[nrules, columns];
+            ruleOrAnd = new List<int>();
+            RuleOutput = new List<int>();
+
+            int filecount = 0;
+            foreach (string s in rulestring)
+            {
+                //arreglo de reglas
+                string[] array = s.Split(' ');
+                for (int x = 0; x < columns; x++)
+                {
+                    array[x] = array[x].Trim(',');
+                    Console.WriteLine(array[x]);
+                    reglas[filecount, x] = int.Parse(array[x]);
+
+                }
+
+                //Aqui se saca si es OR o AND y el output que afecta
+                //  Console.WriteLine("EL VALOR ACTUAL ES " + array[columns+2]);
+                ruleOrAnd.Add(int.Parse(array[columns + 2]));
+                RuleOutput.Add(int.Parse(array[columns - 1]));
+
+                filecount++;
+                if (filecount == nrules)
+                {
+                    
+                    break;
+                }
+            }
+
+            Console.WriteLine(reglas.ToString());
+            return reglas;
+        }
+
+    }
+
 }
